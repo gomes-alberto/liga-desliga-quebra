f51ef08dad9baf027f8c6adbcda4f472c08bbb5f
Gomes Alberto <gomesrick25@gmail.com>
2026-02-25 09:12:47 +0000

    refactor: Refatorar o código do javascript para uma visualização melhor

--- js/main.js
+++ js/main.js
@@ -27,36 +27,3 @@ turnOff.addEventListener('click', lampOff)
 lampada.addEventListener('mouseover', lampOn)
 lampada.addEventListener('mouseleave', lampOff)
 lampada.addEventListener('dblclick', lampBroken)
-
-// --------------CÓDIGO ANTIGO
-// const turnOn = document.getElementById('turnOn');
-// const turnOff = document.getElementById('turnOff');
-// const lampada = document.getElementById('lampada');
-
-// function isLampBroken() {
-//     return lampada.src.indexOf('quebrada') > -1
-// }
-
-// function lampOn() {
-//     if (!isLampBroken()) {
-//         lampada.src = 'images/ligada.jpg';
-//     }
-// }
-
-// function lampOff() {
-//     if (!isLampBroken()) {
-//         lampada.src = 'images/desligada.jpg';
-//     }
-// }
-
-// function lampBroken() {
-//     lampada.src = 'images/quebrada.jpg';
-// }
-
-// turnOn.addEventListener('click', lampOn);
-// turnOff.addEventListener('click', lampOff);
-
-// lampada.addEventListener('mouseover', lampOn)
-// lampada.addEventListener('mouseleave', lampOff)
-
-// lampada.addEventListener('dblclick', lampBroken);
